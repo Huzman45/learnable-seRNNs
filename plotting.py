f139ff7dd5769a95bdcf63ae84746a530f810cc0
Huzman45 <hhuuzzaaiiffaahh@gmail.com>
2025-02-26 14:30:56 +0000

    Implement topology calculation and plotting

--- plotting.py
+++ plotting.py
@@ -2,28 +2,19 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
+import utils
 
-# history = ['RNN_Weight_Matrix', 'accuracy', 'loss', 'val_accuracy', 'val_loss']
-# history[key] = [(mean, std), ...]
-# len(history[key]) = num_epochs
-def load_history(file_path) -> dict[str, list[tuple[float, float]]]:
-    with open(file_path, "rb") as file:
-        history = pickle.load(file)
-
-    return history
-
-
-def plot_histories(
-    histories: list[dict[str, list[tuple[float, float]]]],
+def plot_histories(histories: list[list[dict[list[float]]]],
     legend: list[str],
     metric: str,
     title: str,
 ) -> None:
     plt.figure()
     for history, label in zip(histories, legend):
-        values = history[metric]
-        mean = [value[0] for value in values]
-        std = [value[1] for value in values]
+        vals = np.array([net[metric] for net in history])
+        mean = np.mean(vals, axis=0)
+        std = np.std(vals, axis=0)
+
         epochs = range(1, len(mean) + 1)
         plt.plot(epochs, mean, label=label)
         plt.fill_between(
@@ -38,21 +29,64 @@ def plot_histories(
     plt.legend()
     plt.savefig(f"plots/{title}.png")
 
+def plot_local_statistics(
+    local_statistics: list[np.ndarray], legend: list[str], metric: int, title: str
+) -> None:
+    plt.figure()
+    for statistics, label in zip(local_statistics, legend):
+        mean = np.mean(statistics[:, :, :, metric], axis=(0, 2))
+        std = np.std(statistics[:, :, :, metric], axis=(0, 2))
+        epochs = range(1, len(mean) + 1)
+        plt.plot(epochs, mean, label=label)
+        plt.fill_between(epochs, mean - std, mean + std, alpha=0.2)
+
+    plt.xlabel("Epochs")
+    plt.ylabel(title)
+    plt.legend()
+    plt.savefig(f"plots/{title}.png")
+
+def plot_global_statistics(
+    global_statistics: list[np.ndarray], legend: list[str], metric: int, title: str
+) -> None:
+    plt.figure()
+    for statistics, label in zip(global_statistics, legend):
+        mean = np.mean(statistics[:, :, metric], axis=0)
+        std = np.std(statistics[:, :, metric], axis=0)
+        epochs = range(1, len(mean) + 1)
+        plt.plot(epochs, mean, label=label)
+        plt.fill_between(epochs, mean - std, mean + std, alpha=0.2)
+
+    plt.xlabel("Epochs")
+    plt.ylabel(title)
+    plt.legend()
+    plt.savefig(f"plots/{title}.png")
+
 
 if __name__ == "__main__":
     history_path = "data/history.pickle"
-    history = load_history(history_path)
+    history = utils.load_history(history_path)
+    local_stats = utils.load_local_statistics()
+    global_stats = utils.load_global_statistics()
 
-    history_2 = history.copy()
-    for key in history:
-        history_2[key] = [
-            (mean + np.random.random() - 0.5, std + np.random.random() - 0.5)
-            for mean, std in history[key]
-        ]
+    # plot_histories([history, history_2], ["RNN", "Rand"], "accuracy", "Accuracy")
+    # plot_histories([history, history_2], ["RNN", "Rand"], "loss", "Loss")
+    # plot_histories(
+    #     [history, history_2], ["RNN", "Rand"], "val_accuracy", "Validation Accuracy"
+    # )
+    # plot_histories([history, history_2], ["RNN", "Rand"], "val_loss", "Validation Loss")
 
-    plot_histories([history, history_2], ["RNN", "Rand"], "accuracy", "Accuracy")
-    plot_histories([history, history_2], ["RNN", "Rand"], "loss", "Loss")
-    plot_histories(
-        [history, history_2], ["RNN", "Rand"], "val_accuracy", "Validation Accuracy"
-    )
-    plot_histories([history, history_2], ["RNN", "Rand"], "val_loss", "Validation Loss")
+    plot_local_statistics([local_stats], ["seRNN"], 0, "Strength")
+    plot_local_statistics([local_stats], ["seRNN"], 1, "Clustering")
+    plot_local_statistics([local_stats], ["seRNN"], 2, "Betweenness")
+    plot_local_statistics([local_stats], ["seRNN"], 3, "Weighted Edge Length")
+    plot_local_statistics([local_stats], ["seRNN"], 4, "Communicability")
+    plot_local_statistics([local_stats], ["seRNN"], 5, "Matching")
+
+    plot_global_statistics([global_stats], ["seRNN"], 0, "Total Weight")
+    plot_global_statistics([global_stats], ["seRNN"], 1, "Total Weighted Edge Length")
+    plot_global_statistics([global_stats], ["seRNN"], 2, "Global Efficiency")
+    plot_global_statistics([global_stats], ["seRNN"], 3, "Homophily per Weight")
+    plot_global_statistics([global_stats], ["seRNN"], 4, "Modularity")
+    plot_global_statistics([global_stats], ["seRNN"], 5, "Efficiency per Weight")
+    plot_global_statistics([global_stats], ["seRNN"], 6, "Corr(Weight, Distance)")
+    plot_global_statistics([global_stats], ["seRNN"], 7, "Small Worldness")
---
+++ topology.py
@@ -0,0 +1,81 @@
+import scipy.linalg
+import utils
+import bct
+import numpy as np
+import pickle
+
+
+def small_worldness(binary_weight_matrix: np.ndarray, n_nodes: int = 100) -> float:
+    # Empirical clustering and path length
+    A = binary_weight_matrix
+    clu = np.mean(bct.clustering_coef_bu(A))
+    pth = bct.efficiency_bin(A)
+    # Run nperm null models
+    nperm = 1000
+    cluperm = np.zeros((nperm, 1))
+    pthperm = np.zeros((nperm, 1))
+    for perm in range(nperm):
+        Wperm = np.random.rand(n_nodes, n_nodes)
+        # Make it into a matrix
+        Wperm = np.matrix(Wperm)
+        # Make symmetrical
+        Wperm = Wperm + Wperm.T
+        Wperm = np.divide(Wperm, 2)
+        # Binarise
+        threshold, upper, lower = 0.7, 1, 0
+        Aperm = np.where(Wperm > threshold, upper, lower)
+        # Take null model
+        cluperm[perm] = np.mean(bct.clustering_coef_bu(Aperm))
+        pthperm[perm] = bct.efficiency_bin(Aperm)
+    # Take the average of the nulls
+    clunull = np.mean(cluperm)
+    pthnull = np.mean(pthperm)
+    # Compute the small worldness
+    smw = np.divide(np.divide(clu, clunull), np.divide(pth, pthnull))
+    return smw
+
+
+history: np.ndarray = utils.load_history()
+num_nets = len(history)
+num_epochs = len(history[0]["accuracy"])
+
+# Local statistics = [strength, clustering, betweenness, weighted_edge_length, communicability, matching]
+n_l_stats = 6
+n_nodes = history[0]["weight_matrix"][0].shape[0]
+local_statistics = np.zeros((num_nets, num_epochs, n_nodes, n_l_stats))
+thresh = 0.1
+
+# Global statistics = [total_weight, total_weighted_edge_length, global_efficiency,
+# homophily_per_weight, modularity, efficiency_per_weight, corr(weight,distance), small_worldness]
+n_g_stats = 8
+global_statistics = np.zeros((num_nets, num_epochs, n_g_stats))
+
+for i in range(num_nets):
+    for epoch in range(num_epochs):
+        net: np.ndarray = history[i]["weight_matrix"][epoch]
+        distance: np.ndarray = history[i]["distance_matrix"][0]
+        binarised = bct.threshold_proportional(net, thresh)
+
+        local_statistics[i, epoch, :, 0] = bct.strengths_und(net)
+        local_statistics[i, epoch, :, 1] = bct.clustering_coef_wu(net)
+        local_statistics[i, epoch, :, 2] = bct.betweenness_wei(net)
+        local_statistics[i, epoch, :, 3] = np.sum(net * distance)
+        local_statistics[i, epoch, :, 4] = scipy.linalg.expm(net).mean()
+        local_statistics[i, epoch, :, 5] = np.mean(bct.matching_ind(binarised) * 2)
+
+        global_statistics[i, epoch, 0] = np.sum(net)
+        global_statistics[i, epoch, 1] = (net * distance).mean()
+        global_statistics[i, epoch, 2] = bct.efficiency_wei(net)
+        global_statistics[i, epoch, 3] = np.mean(local_statistics[i, epoch, :, 5].squeeze())/global_statistics[i, epoch, 0]
+        global_statistics[i, epoch, 4] = bct.modularity_und(net)[1]
+        global_statistics[i, epoch, 5] = (
+            global_statistics[i, epoch, 2] / global_statistics[i, epoch, 0]
+        )
+        global_statistics[i, epoch, 6] = np.corrcoef(distance[net > 0], net[net > 0])[0, 1]
+        global_statistics[i, epoch, 7] = small_worldness(binarised, n_nodes)
+
+with open("data/local_statistics.pickle", "wb") as file:
+    pickle.dump(local_statistics, file)
+
+with open("data/global_statistics.pickle", "wb") as file:
+    pickle.dump(global_statistics, file)
